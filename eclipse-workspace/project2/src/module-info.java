3183a1e7ffb78ffd8daa4409eb9d9d88094ba43d
5abdev <sabinoramirez017@gmail.com>
2022-02-07 17:18:28 -0800

    updated dotfiles

---
+++ eclipse-workspace/project2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sabino
+ *
+ */
+module project2 {
+}
---
+++ eclipse-workspace/project2/src/project2/misc.java
@@ -0,0 +1,21 @@
+package project2;
+
+import java.math.BigInteger;
+
+public class misc {
+
+//	BigInteger x = 02;
+//	BigInteger e = 1438807;
+//	BigInteger n = 5679743;
+	
+	
+	public static void main(String[] args) {
+		double x = 2.0;
+		double e = 1438807.0;
+		double n = 5679743.0;
+
+		double y = Math.pow(x, e);
+
+		System.out.println(y);
+	}
+}
